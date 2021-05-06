f72aab31fa276064ed6f6deecfabc98400966690
sreeragps <sreerag@brainchild.services>
2021-05-06 20:14:17 +0530

    p tag test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import { useState } from 'react';
 import './App.css';
-import Home from './Home';
-import Pricing from './Pricing';
 
-// 16.8.0+ >
+
 function App() {
-  // const [state, setState] = useState(initialState)
-
-
-  const [pageView,setPageView] = useState("HOME");
-  // || && >= === ==
-  const changeToPricingPage = () =>{
-    // pageView = "PRICING" ----> Should not Mutate Directly
-    setPageView("PRICING")
-  }
-  const backToHome = () =>{
-    setPageView("HOME")
-  }
+  
   return (
     <div className="App">
-      This is in Sample React App
-      {pageView === "HOME" && (
-        <Home changeToPricingPage={changeToPricingPage} />
-      )}
-      {pageView === "PRICING" && (
-        <Pricing backToHome={backToHome}/>
-      )}
+       <p>I am learning React. My life is getting better.</p>
     </div>
   );
 }
